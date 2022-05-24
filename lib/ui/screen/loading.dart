5726b1c4f93f2fd786f11d2acb2ab978ecdc4f9b
FlutLab.io <>
2022-05-24 06:05:36 +0000

    Autosave

--- lib/ui/screen/loading.dart
+++ lib/ui/screen/loading.dart
@@ -5,6 +5,7 @@ import 'package:somali_recipe/ui/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:somali_recipe/model/recipe_model.dart';
 import 'package:http/http.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class PracLoading extends StatefulWidget {
   _PracLoadingState createState() => _PracLoadingState();
@@ -18,6 +19,13 @@ class _PracLoadingState extends State<PracLoading> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.blue[900], body: Center());
+    return Scaffold(
+        backgroundColor: Colors.blue[900],
+        body: Center(
+          child: SpinKitFoldingCube(
+            color: Colors.white,
+            size: 50.0,
+          ),
+        ));
   }
 }
